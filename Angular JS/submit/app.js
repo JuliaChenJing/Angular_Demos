39ee2d5a2707aa7d2e9bdd40417a66d5aec2f74b
Julia Chen <juliachenjing@gmail.com>
2017-11-18 21:45:14 +0800

    still not adding name

--- Angular JS/submit/app.js
+++ Angular JS/submit/app.js
@@ -2,20 +2,30 @@ var app = angular.module('myApp', []).controller('myCtrl', function ($scope) {
 
     $scope.names = [{ firstname: "Bimal", lastname: "Parajuli" },
     { firstname: "Julia", lastname: "Chen" }
-    ]
+    ];
 
 
-    this.submitName = function () {
-        this.names.push({ firstname: this.object.firstname, lastname: this.object.lastname });
-
-    }
-    $scope.list = [];
+    $scope.list = ["abc"];
     $scope.text = 'hello';
+    $scope.firstName = "firstname";
+    $scope.lastName = "lastname";
     $scope.submitText = function () {
+
         if ($scope.text) {
             $scope.list.push(this.text);
             $scope.text = '';
         }
+    };
+
+
+    $scope.submitName = function () {
+
+
+        $scope.names.push({ firstname: this.firstName, lastname: this.lastName });
+        $scope.firstName = '';
+        $scope.lastName = '';
+
+
     };
 
 
