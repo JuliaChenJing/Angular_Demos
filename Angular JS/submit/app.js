6f683684fe87f519fc3f03a90401ade0cc312ccd
Julia Chen <juliachenjing@gmail.com>
2017-11-18 21:24:17 +0800

    update

--- Angular JS/submit/app.js
+++ Angular JS/submit/app.js
@@ -1,11 +1,22 @@
 var app = angular.module('myApp', []).controller('myCtrl', function ($scope) {
 
     $scope.names = [{ firstname: "Bimal", lastname: "Parajuli" },
-    { firstname: "Julia", lastname: "Chen" }]
+    { firstname: "Julia", lastname: "Chen" }
+    ]
 
 
-    this.submitButton = function () {
+    this.submitName = function () {
         this.names.push({ firstname: this.object.firstname, lastname: this.object.lastname });
-        console("clicked");
+
     }
+    $scope.list = [];
+    $scope.text = 'hello';
+    $scope.submitText = function () {
+        if ($scope.text) {
+            $scope.list.push(this.text);
+            $scope.text = '';
+        }
+    };
+
+
 });
