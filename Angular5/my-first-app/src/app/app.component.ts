552d30fd570ecbf6b1ba315b9a96737db36334b8
Julia Chen <juliachenjing@gmail.com>
2018-01-08 17:33:37 -0500

    styles

--- Angular5/my-first-app/src/app/app.component.ts
+++ Angular5/my-first-app/src/app/app.component.ts
@@ -4,6 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-root',//the name of the component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  /*styles: [`h3 {
+    color:red;
+}
+  `]*/
 })
 export class AppComponent {
   title = 'Julia just changed it';
