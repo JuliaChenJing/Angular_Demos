50b0c438543c87be25fb81b73255e5918ded46f5
Julia Chen <juliachenjing@gmail.com>
2018-01-08 16:58:03 -0500

    added comments

--- Angular5/my-first-app/src/main.ts
+++ Angular5/my-first-app/src/main.ts
@@ -1,3 +1,4 @@
+//first code to get excuted
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -8,5 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)//start this angular application by passing an appModule to this method 
   .catch(err => console.log(err));
