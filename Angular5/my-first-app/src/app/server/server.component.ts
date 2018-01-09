bc487ec4ba44f26b88683ba66a89611342c312fb
Julia Chen <juliachenjing@gmail.com>
2018-01-09 15:37:40 -0500

    [ngClass]

--- Angular5/my-first-app/src/app/server/server.component.ts
+++ Angular5/my-first-app/src/app/server/server.component.ts
@@ -3,22 +3,25 @@ import { ServersComponent } from '../servers/servers.component';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+      .online{color:white;} `
+    ]
 })
 
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'off';
 
-    constructor(){
-        this.serverStatus=Math.random()>0.5? 'online':'offline';
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }
-    getServerStatus(){
+    getServerStatus() {
         return this.serverStatus;
     }
 
-    getColor(){
-        return this.serverStatus==='online'? 'green':'red';
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 
 }
