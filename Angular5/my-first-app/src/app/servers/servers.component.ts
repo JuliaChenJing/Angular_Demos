5da4cd7a887f2bf8089070c2a57cd0930efd627f
Julia Chen <juliachenjing@gmail.com>
2018-01-09 11:02:10 -0500

    event binding to button

--- Angular5/my-first-app/src/app/servers/servers.component.ts
+++ Angular5/my-first-app/src/app/servers/servers.component.ts
@@ -8,13 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer:boolean = false;
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created!;'
 
   constructor() {
     setTimeout(() => { this.allowNewServer = true }, 2000)
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
