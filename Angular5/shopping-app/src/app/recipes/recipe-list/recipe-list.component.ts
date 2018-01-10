18d4ff4b573d4abfe817ebf2cb2bb9339b8ba504
Julia Chen <juliachenjing@gmail.com>
2018-01-10 11:24:25 -0500

    48 outputting a list of recipes with ngFor

--- Angular5/shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular5/shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "simply a test","http://s4.sinaimg.cn/middle/610a6eb2t774f52cd64d3&690")
+    new Recipe("tomato dish", "sweet","https://lh3.ggpht.com/mM9OZNTDb9fOObiNoc-lM9D4olwN59RRTvAKTxJ9Nu410gJqeEUJFEk9B9p2CTwFdA=h900"), 
+    new Recipe("potato dish", "spicy","https://lh3.googleusercontent.com/pUcUtJ513GkOE-w20ERCXXY7n6ABM_8JnYVsLKajlvu0K8CWLyFGdU28-gDgNcEg9sg=h900"),
+    new Recipe("cake", "tasty","http://cdn.apk-cloud.com/detail/screenshot/aQY9ihz4rHU-Xhe8kqgW-Jw5ZcJoAC076y0iXDnpwNBRRdjeMIWZeiWpInBlvreLBNw=h900.png")
   ];
 
   constructor() { }
