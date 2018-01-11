2bd4b12073e6e9ee70ff90fa4ec8cfbfbf0ddb61
Julia Chen <juliachenjing@gmail.com>
2018-01-11 17:29:17 -0500

    67 more on view encapsulation

--- Angular5/section5-databinding-start/src/app/server-element/server-element.component.ts
+++ Angular5/section5-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core/src/metadata/view';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None
+  //Native (only in the browser supporting Shadow Dom)
+  // Emulated(default, Shadow DOM)
+  //None (No Shadow Dom),global effection
 })
 export class ServerElementComponent implements OnInit {
 
