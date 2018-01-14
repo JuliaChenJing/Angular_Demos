c03720bb3bf215fa1cf05892efd62c34926c51b4
Julia Chen <juliachenjing@gmail.com>
2018-01-14 18:51:12 -0500

    just change the cake picture

--- Angular5/recipesAndShippingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular5/recipesAndShippingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("tomato dish", "sweet", "https://lh3.ggpht.com/mM9OZNTDb9fOObiNoc-lM9D4olwN59RRTvAKTxJ9Nu410gJqeEUJFEk9B9p2CTwFdA=h900"),
     new Recipe("potato dish", "spicy", "https://lh3.googleusercontent.com/pUcUtJ513GkOE-w20ERCXXY7n6ABM_8JnYVsLKajlvu0K8CWLyFGdU28-gDgNcEg9sg=h900"),
-    new Recipe("cake", "tasty", "http://cdn.apk-cloud.com/detail/screenshot/aQY9ihz4rHU-Xhe8kqgW-Jw5ZcJoAC076y0iXDnpwNBRRdjeMIWZeiWpInBlvreLBNw=h900.png")
+    new Recipe("cake", "tasty", "https://gw.alicdn.com/bao/uploaded/i4/T1_y_uXklrXXayLDI8_101733.jpg_460x460xz.jpg")
   ];
 
   constructor() { }
