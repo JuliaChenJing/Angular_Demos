7092068a80ec6d74e403d74a6387660684e8a174
Julia Chen <juliachenjing@gmail.com>
2018-01-14 20:55:35 -0500

    78 property binding~~~~~~~~~~~~~~~~~~

--- Angular5/recipesAndShippingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular5/recipesAndShippingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
   recipes: Recipe[] = [
     new Recipe("tomato dish", "sweet", "https://lh3.ggpht.com/mM9OZNTDb9fOObiNoc-lM9D4olwN59RRTvAKTxJ9Nu410gJqeEUJFEk9B9p2CTwFdA=h900"),
     new Recipe("potato dish", "spicy", "https://lh3.googleusercontent.com/pUcUtJ513GkOE-w20ERCXXY7n6ABM_8JnYVsLKajlvu0K8CWLyFGdU28-gDgNcEg9sg=h900"),
