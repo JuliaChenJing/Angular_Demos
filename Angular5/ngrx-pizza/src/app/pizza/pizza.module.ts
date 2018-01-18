bab316c94d408503c1ad1f1b2fe87084abbb4d30
Julia Chen <juliachenjing@gmail.com>
2018-01-17 19:27:27 -0500

    ng g component pizza/pizza-order -m pizza

--- Angular5/ngrx-pizza/src/app/pizza/pizza.module.ts
+++ Angular5/ngrx-pizza/src/app/pizza/pizza.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PizzaOrderComponent } from './pizza-order/pizza-order.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PizzaOrderComponent]
 })
 export class PizzaModule { }
