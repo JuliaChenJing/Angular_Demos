8a733eb75bd09d990c1dd2c5e23de038a5d599a7
Julia Chen <juliachenjing@gmail.com>
2018-01-18 10:06:48 -0500

    'component' is not a known element: solution 2 Declare the showing component in stage module

--- Angular5/ngrx-pizza/src/app/pizza/pizza.module.ts
+++ Angular5/ngrx-pizza/src/app/pizza/pizza.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PizzaOrderComponent } from './pizza-order/pizza-order.component';
 
 import {StoreModule} from '@ngrx/store';
 import {pizzaReducer} from './pizza.reducer';
@@ -10,7 +9,6 @@ import {pizzaReducer} from './pizza.reducer';
     CommonModule,
     StoreModule.forFeature('pizza',pizzaReducer),
   ],
-  declarations: [PizzaOrderComponent],
-  exports: [PizzaOrderComponent]
+
 })
 export class PizzaModule { }
