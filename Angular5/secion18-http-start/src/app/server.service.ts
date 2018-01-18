e0a66b3ef26312d79246dd29ae7459ab6a2fae1f
Julia Chen <juliachenjing@gmail.com>
2018-01-18 13:32:17 -0500

    237 sending a put request

--- Angular5/secion18-http-start/src/app/server.service.ts
+++ Angular5/secion18-http-start/src/app/server.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http ,Headers} from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 /*this decorator is required if you plan on injecting a service into a servie--- 
 inject the build in  angular http service, which gives us some methods we need for
@@ -8,18 +8,16 @@ inject the build in  angular http service, which gives us some methods we need f
 @Injectable()
 export class ServerService {
 
-    constructor(private http: Http) {}
+    constructor(private http: Http) { }
 
     storeServers(servers: any[]) {
-
-        const headers=new Headers({'Content-Type':"application/json"});
-
+        const headers = new Headers({ 'Content-Type': "application/json" });
         return this.http.put('https://udemy-ng-http-2d953.firebaseio.com/data.json',
-         servers,
-         {headers:headers});
+            servers,
+            { headers: headers });
     }
 
-    getServers(){
+    getServers() {
         return this.http.get('https://udemy-ng-http-2d953.firebaseio.com/data.json')
     }
 }
