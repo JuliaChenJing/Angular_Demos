d3fc260afc3678ad6fa4a1d82fb4ccd1a422bb49
Julia Chen <juliachenjing@gmail.com>
2018-01-18 10:43:33 -0500

    added notes: import, export ,declare

--- Angular5/ngrx-pizza/src/app/pizza/pizza.module.ts
+++ Angular5/ngrx-pizza/src/app/pizza/pizza.module.ts
@@ -7,12 +7,21 @@ import { pizzaReducer } from './pizza.reducer';
 import { PizzaOrderComponent } from './pizza-order/pizza-order.component';
 
 @NgModule({
+
+  //imports makes the exported declarations of other modules available in the current module
   imports: [
     CommonModule,
     StoreModule.forFeature('pizza', pizzaReducer),
   ],
 
+  /*declarations are to make directives (including components and pipes) from the CURRENT MODEL
+   available to other directives in the current module. Selectors of directives, 
+   components or pipes are only matched against the HTML if they are declared or imported.*/
   declarations: [PizzaOrderComponent],
+
+  /*exports makes the components, directives, and pipes available in modules that add this 
+  module to imports.  exports can also be used to re-export modules such as CommonModule 
+  and FormsModule, which is often done in shared modules.*/
   exports: [PizzaOrderComponent]
 
 })
