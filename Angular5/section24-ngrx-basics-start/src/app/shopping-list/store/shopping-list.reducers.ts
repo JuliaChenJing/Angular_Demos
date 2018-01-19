cff09117da2a84f8fed54b3269a77a75e1973040
Julia Chen <juliachenjing@gmail.com>
2018-01-18 19:54:33 -0500

    finished 305 Finishing the First Reducer

--- Angular5/section24-ngrx-basics-start/src/app/shopping-list/store/shopping-list.reducers.ts
+++ Angular5/section24-ngrx-basics-start/src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,8 +1,8 @@
-//action is to be dispatched (派遣，发送)
-import { Action } from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.model';
-
-
+/*import everything that is exported from  shopping-list.acitons.ts file, 
+and store it in a ShoppingListActions variable as one JavaScript Object
+*/
+import * as Shopping_List_Actions from './shopping-list.actions'
 
 //we need to define some initial state for the first run
 const initialState = {
@@ -15,17 +15,17 @@ the arguments will be passed into the function automatically by @ngrx.
 I will show you how to dispatch the action to trigger the reducer through out this module.
 
 A Reducer function will be triggered whenever an anction is dispatched */
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(state = initialState, action: Shopping_List_Actions.ShoppingListActions) {
     // you typically use a switch statement to determine which type of action was dispatched
     switch (action.type) {
 
-        case ADD_INGREDIENT:
+        case Shopping_List_Actions.ADD_INGREDIENT:
             return {
                 ...state,/*this is the spread operators, taking all the old state parameters, 
             so that all properties of the old object will be added on new object*/
-                ingredient: [...state.ingredients, action]
+                ingredient: [...state.ingredients, action.payload]
             }
-
+        default: return state;
     }
 
     /*behind this scenes,@ngrxwill replace the state with the new one
