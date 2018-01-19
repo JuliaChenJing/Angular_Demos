f0e3996b4246aa4049947d7eba86a50ce4db0c3b
Julia Chen <juliachenjing@gmail.com>
2018-01-19 14:36:42 -0500

    308 dispatch actions 3:41  add ingredient function not working as expected

--- Angular5/section24-ngrx-basics-start/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular5/section24-ngrx-basics-start/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,11 +4,13 @@ import {
   OnDestroy,
   ViewChild
 } from '@angular/core';
+
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
-
+import { Store } from "@ngrx/store";
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import * as ShoppingListActions from '../store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -22,20 +24,20 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   editedItemIndex: number;
   editedItem: Ingredient;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService, private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) { }
 
   ngOnInit() {
     this.subscription = this.slService.startedEditing
       .subscribe(
-        (index: number) => {
-          this.editedItemIndex = index;
-          this.editMode = true;
-          this.editedItem = this.slService.getIngredient(index);
-          this.slForm.setValue({
-            name: this.editedItem.name,
-            amount: this.editedItem.amount
-          })
-        }
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
+      }
       );
   }
 
@@ -44,8 +46,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
       this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      console.log("edit mode");
     } else {
-      this.slService.addIngredient(newIngredient);
+      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
+      console.log("not edit mode");
     }
     this.editMode = false;
     form.reset();
--- Angular5/section24-ngrx-basics-start/src/app/shopping-list/store/shopping-list.actions.ts
+++ Angular5/section24-ngrx-basics-start/src/app/shopping-list/store/shopping-list.actions.ts
@@ -4,8 +4,12 @@ import { Ingredient } from '../../shared/ingredient.model';
 export const ADD_INGREDIENT = "ADD_INGREDIENT";//Uppercase is better
 
 export class AddIngredient implements Action {
-    readonly type = ADD_INGREDIENT;//forced to provide a readonly type property because of Action 
-    payload: Ingredient;
+
+    readonly type = ADD_INGREDIENT;//forced to provide a readonly type property because of Action
+    
+    //by adding this constructor, I want to use the typescript short cut of turing this 
+    //argument into a property of this class by adding an accesory in front of it 
+    constructor(public payload) {}
 }
 
 export type ShoppingListActions = AddIngredient;
