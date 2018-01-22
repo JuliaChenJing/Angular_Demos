869b8d655c9c6e3e975f16474b666453acf1ca20
Julia Chen <juliachenjing@gmail.com>
2018-01-21 21:01:48 -0500

    to run the test: ng test

--- Angular5/ng5-jasmin-karma-test/src/app/app.component.spec.ts
+++ Angular5/ng5-jasmin-karma-test/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+//This is  how ProTractor understands there is a test for it.
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
@@ -8,16 +9,25 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  /*
+  AppComponent
+  should create the app
+  should have as title 'app'
+  should render title in a h1 tag
+  */
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
