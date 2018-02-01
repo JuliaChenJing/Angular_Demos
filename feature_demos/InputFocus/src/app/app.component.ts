0e4fa840ad9c04022a10d83fbb7003494687baf8
Julia Chen <juliachenjing@gmail.com>
2018-02-01 13:44:27 -0500

    worked

--- feature_demos/InputFocus/src/app/app.component.ts
+++ feature_demos/InputFocus/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  setSelectionRange(input, selectionStart, selectionEnd) {
+    if (input.setSelectionRange) {
+      input.focus();
+      input.setSelectionRange(selectionStart, selectionEnd);
+    } else if (input.createTextRange) {
+      var range = input.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', selectionEnd);
+      range.moveStart('character', selectionStart);
+      range.select();
+    }
+  }
 }
